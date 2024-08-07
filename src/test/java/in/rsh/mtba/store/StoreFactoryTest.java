09460ab54b7fd270a7318515ac4c26c1f575efe0
Rahil S <rahilrshk@gmail.com>
2024-08-07 20:13:34 +0530

    Upgraded junit to 5

--- src/test/java/in/rsh/mtba/store/StoreFactoryTest.java
+++ src/test/java/in/rsh/mtba/store/StoreFactoryTest.java
@@ -1,14 +1,14 @@
 package in.rsh.mtba.store;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import in.rsh.mtba.model.Theatre;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class StoreFactoryTest {
+class StoreFactoryTest {
 
   @Test
-  public void testStoreFactory() {
+  void storeFactory() {
     GenericStore<Theatre> store = StoreFactory.getInstance().getStore(Theatre.class);
     Theatre theatre = Theatre.builder().theatreId(1).build();
     store.put(theatre.getTheatreId(), theatre);
